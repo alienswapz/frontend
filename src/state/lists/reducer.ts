1debb39c450bafc9cf45e17509228610170e6a7b
alienswap <alienswapz@protonmail.com>
2024-08-26 12:33:18 -0400

    removed uniswap list

--- src/state/lists/reducer.ts
+++ src/state/lists/reducer.ts
@@ -4,7 +4,8 @@ import { TokenList } from '@uniswap/token-lists/dist/types'
 import { DEFAULT_LIST_OF_LISTS, DEFAULT_TOKEN_LIST_URL } from '../../constants/lists'
 import { updateVersion } from '../global/actions'
 import { acceptListUpdate, addList, fetchTokenList, removeList, selectList } from './actions'
-import UNISWAP_DEFAULT_LIST from '@uniswap/default-token-list'
+//import UNISWAP_DEFAULT_LIST from '@uniswap/default-token-list'
+//import data from 'tokenlist.json'
 //import fetch from 'node-fetch';
 
 export interface ListsState {
@@ -53,7 +54,7 @@ const initialState: ListsState = {
     }, {}),
     [DEFAULT_TOKEN_LIST_URL]: {
       error: null,
-      current: UNISWAP_DEFAULT_LIST,
+      current: null,
       loadingRequestId: null,
       pendingUpdate: null
     }
